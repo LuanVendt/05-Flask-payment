b5df9c6c2426bb1dca255543596e0f7c399d4389
Luan Vendt <luanvendt@gmail.com>
2024-02-21 16:12:26 -0300

    Armazenando qrcode e criando rota de exibição de imagens

--- db_models/payment.py
+++ db_models/payment.py
@@ -5,7 +5,7 @@ class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float)
     paid = db.Column(db.Boolean, default=False)
-    bank_payment_id = db.Column(db.Integer, nullable=True)
+    bank_payment_id = db.Column(db.String(200), nullable=True)
     qr_code = db.Column(db.String(100), nullable=True)
     expiration_date = db.Column(db.DateTime)
 
@@ -14,7 +14,7 @@ class Payment(db.Model):
             'id': self.id,
             'value': self.value,
             'paid': self.paid,
-            'bank_paument_id': self.bank_payment_id,
+            'bank_payment_id': self.bank_payment_id,
             'qr_code': self.qr_code,
             'expiration_date': self.expiration_date
         }
